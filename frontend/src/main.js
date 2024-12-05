b072f3011a8d6801718f95d0cbd90e1886b10eef
unknown <datp1907@gmail.com>
2024-12-05 08:07:35 +0700

    deploy

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import Toaster from "@meforma/vue-toaster";
-import Client from './Layout/Wrapper/MasterClient.vue';
+import Client from '@/Layout/Wrapper/MasterClient.vue';
 import Auth from '@/Layout/Wrapper/AuthClient.vue';
 import Default from './Layout/Wrapper/MasterRocker.vue';
 import Admin from './Layout/Wrapper/MasterAdmin.vue';
